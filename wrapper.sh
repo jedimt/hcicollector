36553ce91ad1a6913b898519a8dd01903089ee05
Aaron Patten <aaronpatten@gmail.com>
2017-06-26 09:35:33 -0600

    added collector to docker-compose config

--- wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-while true
-do
- /usr/bin/python /solidfire_graphite_collector_v2.py -s 172.27.40.200 -u admin -p solidfire -g 172.17.0.2
- /usr/bin/python /solidfire_graphite_collector_v2.py -s 172.27.40.205 -u admin -p solidfire -g 172.17.0.2 
-sleep 60
-done
-
